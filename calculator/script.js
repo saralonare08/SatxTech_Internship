f2f51e1a1a97d99e72eaacc95887819eedbef091
saralonare08 <153102498+saralonare08@users.noreply.github.com>
2025-06-20 20:05:26 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,18 @@
+// script.js
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
